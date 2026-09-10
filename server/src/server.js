dda6d7e9619b2732a2a243263890152b7450078e
SantiagoChavez <chavezsantiago480@gmail.com>
2026-09-10 14:41:32 -0300

    fix(server): validar MONGODB_URI y cargar dotenv al inicio del servidor

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import app from './app.js';
 import { connectDB } from './config/db.js';
-
-// Cargar variables de entorno
-dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
